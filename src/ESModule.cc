c7efc56fb1f5238033acf19bdf097c1d332e6140
engsr6982 <pwxg286704@163.com>
2025-08-13 22:52:01 +0800

    refactor(esmodule): optimize module export initialization

--- src/ESModule.cc
+++ src/ESModule.cc
@@ -15,26 +15,27 @@ ESModuleDefine::ESModuleDefine(std::string name, std::vector<ClassDefine const*>
   class_(std::move(class_)) {}
 
 JSModuleDef* ESModuleDefine::init(JsEngine* engine) const {
-    return JS_NewCModule(engine->context_, name_.c_str(), [](JSContext* ctx, JSModuleDef* module) -> int {
+    auto module = JS_NewCModule(engine->context_, name_.c_str(), [](JSContext* ctx, JSModuleDef* module) -> int {
         auto engine = static_cast<JsEngine*>(JS_GetContextOpaque(ctx));
         auto iter   = engine->nativeESModules_.find(module);
         assert(iter != engine->nativeESModules_.end());
 
         auto& def = iter->second;
-
         for (auto& c : def->class_) {
             auto iter = engine->nativeClassData_.find(c);
             assert(iter != engine->nativeClassData_.end());
 
             auto ctor = iter->second.first;
-            int  code = JS_AddModuleExport(ctx, module, c->name_.c_str());
-            JsException::check(code);
-            code = JS_SetModuleExport(ctx, module, c->name_.c_str(), JS_DupValue(ctx, ctor));
+            int  code = JS_SetModuleExport(ctx, module, c->name_.c_str(), JS_DupValue(ctx, ctor));
             JsException::check(code);
         }
-
         return 0;
     });
+    for (auto& c : class_) {
+        int code = JS_AddModuleExport(engine->context_, module, c->name_.c_str());
+        JsException::check(code);
+    }
+    return module;
 }
 
 
