e1d9e92fcd96a0d72974cd5a64e8e32ae18d189a
engsr6982 <pwxg286704@163.com>
2025-09-10 21:20:11 +0800

    feat(types): add TypeScript type definitions for C++ containers

---
+++ qjspp.d.ts
@@ -0,0 +1,43 @@
+/**
+ * C++ std::optional
+ */
+declare type optional<T> = T | null;
+
+/**
+ * C++ std::vector
+ */
+declare type vector<T> = Array<T>;
+
+/**
+ * C++ std::unordered_map
+ */
+declare type unordered_map<K extends string, V> = {
+  [key: string]: V;
+};
+
+/**
+ * C++ std::variant
+ * @example type MyType = variant<[number, string]>;
+ */
+declare type variant<Args extends readonly unknown[]> = Args[number];
+
+/**
+ * C++ enum (qjspp)
+ * @example
+ * export type EventPriority = NativeEnum<["Highest", "High", "Normal", "Low", "Lowest"]>;
+ */
+declare type NativeEnum<Keys extends readonly string[]> = {
+  readonly $name: string; // qjspp generated
+} & {
+  readonly [K in Keys[number]]: number;
+};
+
+/**
+ * C++ class
+ * @template T
+ * - T: Class
+ * @example class MyClass implements InstanceClassHelper<MyClass> { ... }
+ */
+declare interface InstanceClassHelper<T> {
+  $equals(other: T): boolean;
+}
